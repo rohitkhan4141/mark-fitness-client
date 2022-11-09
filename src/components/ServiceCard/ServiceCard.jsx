35d07b5eb58f27af99c98d4ae1a4050fd14cdd49
rohitkhan4141 <rohitkhan4141@gmail.com>
2022-11-09 20:29:44 +0600

    photo view functionality added

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
   return (
     <div className='card w-80 bg-base-100 shadow-2xl text-white'>
-      <figure>
-        <img className='card-img' src={service?.img} alt='Shoes' />
-      </figure>
+      <PhotoProvider>
+        <PhotoView src={service?.img}>
+          <img
+            className='card-img rounded-lg cursor-pointer'
+            src={service?.img}
+            alt='Shoes'
+          />
+        </PhotoView>
+      </PhotoProvider>
       <div className='card-body'>
         <h2 className='card-title'>{service?.name}</h2>
         <p>{service?.description?.slice(0, 50) + "...  "} </p>
